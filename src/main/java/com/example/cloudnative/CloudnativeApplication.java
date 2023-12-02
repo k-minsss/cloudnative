ebb7a013fdbbedea35d5a182126670741fb8acd2
cloud <cloud1382@gmail.com>
2023-12-02 15:39:47 +0900

    initialize

---
+++ src/main/java/com/example/cloudnative/CloudnativeApplication.java
@@ -0,0 +1,19 @@
+package com.example.cloudnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@RestController
+@SpringBootApplication
+public class CloudnativeApplication {
+
+	@RequestMappping("/")
+	String home() {
+		return "Hello world!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnativeApplication.class, args);
+	}
+
+}
